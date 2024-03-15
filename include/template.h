626cc1ac0f35510fca2cc2403f0fad8d35e5d62b
fabseman <fabse_utilities@pm.me>
2024-03-15 10:00:14 +0100

    Added template .c and .h files

---
+++ include/template.h
@@ -0,0 +1,49 @@
+/*****************************************************************************
+* University of Southern Denmark
+* Project in Cyber Physical Systems (CPS)
+*
+* MODULENAME.: template.h (CHANGE ME)
+*
+* PROJECT....: Traffic light
+*
+* DESCRIPTION: Template for header files (CHANGE ME)
+*
+* Change Log:
+******************************************************************************
+* Date    Id    Change
+* DDMMYY
+* --------------------
+* 150324  MoH    Module created (CHANGE ME)
+*
+*****************************************************************************/
+
+#ifndef _TEMPLATE_H_ // CHANGE ME
+#define _TEMPLATE_H_ // CHANGE ME
+
+/***************************** Include files *******************************/
+
+/*****************************    Defines    *******************************/
+
+/*****************************   Constants   *******************************/
+
+/*****************************   Variables   *******************************/
+
+/*****************************   Functions   *******************************/
+
+void temp_function(); // CHANGE ME
+/*****************************************************************************
+*   Input    : -
+*   Output   : -
+*   Function : Temporary function (CHANGE ME)
+******************************************************************************/
+
+void temp_function_with_input(int temp); // CHANGE ME
+/*****************************************************************************
+*   Input    : -
+*   Output   : -
+*   Function : Temporary function with input (CHANGE ME)
+******************************************************************************/
+
+/***************** End of module **************/
+
+#endif
