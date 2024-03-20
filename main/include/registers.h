fc08829a77f3407a30833c3d00f6993e062de986
fabseman <fabse_utilities@pm.me>
2024-03-20 12:09:23 +0100

    Adding config for code-formatting

--- main/include/registers.h
+++ main/include/registers.h
@@ -38,12 +38,12 @@
 //
 //***************************************************************
 
-#define GPIO_OUT_W1TS_REG_R (*((volatile uint32_t *)GPIO_OUT_W1TS_REG))
-#define GPIO_OUT_W1TC_REG_R (*((volatile uint32_t *)GPIO_OUT_W1TC_REG))
+#define GPIO_OUT_W1TS_REG_R    (*((volatile uint32_t *)GPIO_OUT_W1TS_REG))
+#define GPIO_OUT_W1TC_REG_R    (*((volatile uint32_t *)GPIO_OUT_W1TC_REG))
 
-#define GPIO_IN_REG_R (*((volatile uint32_t *)GPIO_IN_REG))
+#define GPIO_IN_REG_R          (*((volatile uint32_t *)GPIO_IN_REG))
 
-#define GPIO_ENABLE_REG_R (*((volatile uint32_t *)GPIO_ENABLE_REG))
+#define GPIO_ENABLE_REG_R      (*((volatile uint32_t *)GPIO_ENABLE_REG))
 #define GPIO_ENABLE_W1TS_REG_R (*((volatile uint32_t *)GPIO_ENABLE_W1TS_REG))
 #define GPIO_ENABLE_W1TC_REG_R (*((volatile uint32_t *)GPIO_ENABLE_W1TC_REG))
 
@@ -54,7 +54,29 @@
 //***************************************************************
 
 #define UART_CLKDIV_REG_R (*((volatile uint32_t *)UART_CLKDIV_REG))
-#define UART_CONF0_REG_R (*((volatile uint32_t *)UART_CONF0_REG))
+#define UART_CONF0_REG_R  (*((volatile uint32_t *)UART_CONF0_REG))
+
+//***************************************************************
+//
+// System Timer registers (PDF-page 321)
+//
+//***************************************************************
+
+#define SYSTIMER_CONF_REG_R     (*((volatile uint32_t *)SYSTIMER_CONF_REG))
+#define SYSTIMER_LOAD_REG_R     (*((volatile uint32_t *)SYSTIMER_LOAD_REG))
+#define SYSTIMER_LOAD_HI_REG_R  (*((volatile uint32_t *)SYSTIMER_LOAD_HI_REG))
+#define SYSTIMER_LOAD_LO_REG_R  (*((volatile uint32_t *)SYSTIMER_LOAD_LO_REG))
+#define SYSTIMER_VALUE_HI_REG_R (*((volatile uint32_t *)SYSTIMER_LOAD_HI_REG))
+#define SYSTIMER_VALUE_LO_REG_R (*((volatile uint32_t *)SYSTIMER_LOAD_LO_REG))
+#define SYSTIMER_UPDATE_REG_R   (*((volatile uint32_t *)SYSTIMER_UPDATE_REG))
+#define SYSTIMER_TARGET0_CONF_REG_R                                            \
+  (*((volatile uint32_t *)SYSTIMER_TARGET0_CONF_REG))
+#define SYSTIMER_INT_ENA_REG_R (*((volatile uint32_t *)SYSTIMER_INT_ENA_REG))
+#define SYSTIMER_INT_CLR_REG_R (*((volatile uint32_t *)SYSTIMER_INT_CLR_REG))
+#define SYSTIMER_TARGET0_CONF_REG_R                                            \
+  (*((volatile uint32_t *)SYSTIMER_TARGET0_CONF_REG))
+#define SYSTIMER_STEP_REG_R        (*((volatile uint32_t *)SYSTIMER_STEP_REG))
+#define SYSTEM_PERIP_CLK_EN0_REG_R (*((volatile uint32_t *)0x3F4C0040))
 
 /***************** End of module **************/
 
--- main/src/systick.c
+++ main/src/systick.c
@@ -16,6 +16,9 @@
 
 /***************** Include files **************/
 
+#include "../include/systick.h"
+#include "../include/registers.h"
+
 /***************** Defines ********************/
 
 /***************** Constants ******************/
@@ -24,10 +27,24 @@
 
 /***************** Functions ******************/
 
+void enable_clocks() {
+  /*****************************************************************************
+   *   Function : See module specification (.h-file)
+   *****************************************************************************/
+
+  SYSTEM_PERIP_CLK_EN0_REG_R =
+      0x20000405; // Enable peripheral clock for system timer, uart, pcnt
+                  // & timers (pdf-page 410)
+}
+
 void init_systick() {
   /*****************************************************************************
    *   Function : See module specification (.h-file)
    *****************************************************************************/
-}
 
-/***************** End of module **************/
+  SYSTIMER_INT_CLR_REG_R =
+      0x00000000; // clear interrupt on system timers 0, 1 & 2
+  SYSTIMER_UPDATE_REG_R =
+      0x80000000; // enable updating timer values into registers
+  SYSTIMER_INT_ENA_REG_R = 0x00000001; // enable interrupt on system timer 1
+}
